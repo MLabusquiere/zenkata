3d4f3c7dc6574f8e44178ca1e9eaa0f85692877c
MLabusquiere <labusquiere@gmail.com>
2016-10-19 03:56:39 +0200

    Refactor stringsum test instantiation

--- stringsum/src/test/java/io/maxlab/stringsum/StringSumTest.java
+++ stringsum/src/test/java/io/maxlab/stringsum/StringSumTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.assertEquals;
  * Created by maxence on 19/10/16.
  */
 public class StringSumTest {
-
     private StringSum stringSum;
 
     @Before
@@ -18,33 +17,28 @@ public class StringSumTest {
     }
 
     @Test
-    public void empty_string_test()  {
-        final StringSum stringSum = new StringSum();
+    public void empty_string_test() {
         assertEquals("If the string is not a natural number replace it by 0", 0, stringSum.sum("", ""));
     }
 
     @Test
-    public void nominal_case_test()  {
-        final StringSum stringSum = new StringSum();
+    public void nominal_case_test() {
         assertEquals("Expecting the right sum", 3, stringSum.sum("2", "1"));
     }
 
     @Test
-    public void number_but_not_natural_test()  {
-        final StringSum stringSum = new StringSum();
+    public void number_but_not_natural_test() {
         assertEquals("Expecting floating to be counted as 0", 1, stringSum.sum("2.0", "1"));
     }
 
 
     @Test
-    public void integer_but_not_positive_test()  {
-        final StringSum stringSum = new StringSum();
+    public void integer_but_not_positive_test() {
         assertEquals("Expecting negative to be counted as 0", 1, stringSum.sum("-2", "1"));
     }
 
     @Test
-    public void trim_test()  {
-        final StringSum stringSum = new StringSum();
+    public void trim_test() {
         assertEquals("Expect argument to be trimmed", 3, stringSum.sum("2 ", " 1"));
     }
 }
