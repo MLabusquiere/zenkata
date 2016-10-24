25231f459a889985e407faff5af1d93dc571f108
MLabusquiere <labusquiere@gmail.com>
2016-10-24 17:47:59 +0200

    Handle one number in the string calculatorx

--- stringcalculator/src/main/java/io/maxlab/stringsum/StringCalculator.java
+++ stringcalculator/src/main/java/io/maxlab/stringsum/StringCalculator.java
@@ -4,7 +4,11 @@ package io.maxlab.stringsum;
  * Created by maxence on 24/10/16.
  */
 public class StringCalculator {
-    public int get(String s) {
-        return 0;
+    public int get(String number) {
+        try {
+            return Integer.valueOf(number);
+        } catch (NumberFormatException e)   {
+            return 0;
+        }
     }
 }
--- stringcalculator/src/test/java/StringCalculatorTest.java
+++ stringcalculator/src/test/java/StringCalculatorTest.java
@@ -16,7 +16,13 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void emptyString() throws Exception {
-        assertEquals("Empty string should return 0",0,stringCalculator.get(""));
+    public void empty_string() throws Exception {
+        assertEquals("Empty string should return 0", 0, stringCalculator.get(""));
+    }
+
+
+    @Test
+    public void one_number() throws Exception {
+        assertEquals("If there is only one number it should be the result", 1, stringCalculator.get("1"));
     }
 }
