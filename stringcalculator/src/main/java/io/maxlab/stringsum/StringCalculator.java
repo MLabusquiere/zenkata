6d74ecfe8bd1c5905a1f1c9186d6a8f4ebd9fc9f
MLabusquiere <labusquiere@gmail.com>
2016-10-24 18:08:39 +0200

    Handle several numbers

--- stringcalculator/src/main/java/io/maxlab/stringsum/StringCalculator.java
+++ stringcalculator/src/main/java/io/maxlab/stringsum/StringCalculator.java
@@ -4,11 +4,14 @@ package io.maxlab.stringsum;
  * Created by maxence on 24/10/16.
  */
 public class StringCalculator {
-    public int get(String number) {
-        try {
-            return Integer.valueOf(number);
-        } catch (NumberFormatException e)   {
-            return 0;
+    public int add(String numbers) {
+        Integer result = 0;
+        for (String number : numbers.split(",")) {
+            try {
+                result += Integer.valueOf(number);
+            } catch (NumberFormatException ignore) {
+            }
         }
+        return result;
     }
 }
--- stringcalculator/src/test/java/StringCalculatorTest.java
+++ stringcalculator/src/test/java/StringCalculatorTest.java
@@ -17,12 +17,23 @@ public class StringCalculatorTest {
 
     @Test
     public void empty_string() throws Exception {
-        assertEquals("Empty string should return 0", 0, stringCalculator.get(""));
+        assertEquals("Empty string should return 0", 0, stringCalculator.add(""));
     }
 
 
     @Test
     public void one_number() throws Exception {
-        assertEquals("If there is only one number it should be the result", 1, stringCalculator.get("1"));
+        assertEquals("If there is only one number it should be the result", 1, stringCalculator.add("1"));
+    }
+
+    @Test
+    public void two_number() throws Exception {
+        assertEquals("If there is only one number it should be the result", 3, stringCalculator.add("1,2"));
+    }
+
+
+    @Test
+    public void three_number() throws Exception {
+        assertEquals("If there is only one number it should be the result", 6, stringCalculator.add("1,2,3"));
     }
 }
